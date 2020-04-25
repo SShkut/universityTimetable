c3e9834df2203a4939f8148d6a1e81506ed28e13
sshkut <sgshkut@gmail.com>
2020-04-25 22:50:26 +0200

    split templates for test and production

--- src/main/java/com/foxminded/university_timetable/util/SpringJdbcConfig.java
+++ src/main/java/com/foxminded/university_timetable/util/SpringJdbcConfig.java
@@ -2,33 +2,56 @@ package com.foxminded.university_timetable.util;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
-@ComponentScan("com.foxminded.university_timetable.dao")
+@ComponentScan("com.foxminded.university_timetable")
+@PropertySource(value = { "classpath:application.properties" })
 public class SpringJdbcConfig {
+	
+	@Autowired
+	private Environment environment;
 
-	@Bean
+	@Bean("postgresDataSource")
 	public DataSource postgresDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("");
-		dataSource.setUsername("admin");
-		dataSource.setPassword("admin");
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
 		
 		return dataSource;
 	}
 	
-	@Bean
+	@Bean("testDataSource")
 	public DataSource embeddedDataSource() {
 		return new EmbeddedDatabaseBuilder()
 				.setType(EmbeddedDatabaseType.H2)
 				.addScript("schema")
-				.addScript("test-data").build();
+				.build();
+	}
+	
+	@Bean("postgresJdbcTemplate")
+	public JdbcTemplate jdbcTemplate(@Autowired @Qualifier("postgresDataSource") DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.setResultsMapCaseInsensitive(true);
+		return jdbcTemplate;
+	}
+	
+	@Bean("testJdbcTemplate")
+	public JdbcTemplate testJdbcTemplate(@Autowired @Qualifier("testDataSource") DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.setResultsMapCaseInsensitive(true);
+		return jdbcTemplate;
 	}
 }
