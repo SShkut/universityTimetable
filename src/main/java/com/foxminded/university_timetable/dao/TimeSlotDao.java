f673117ee222ac3c4c489cd7c532bd30c7d529a6
sshkut <sgshkut@gmail.com>
2020-05-27 21:15:03 +0200

    Add some tests.

--- src/main/java/com/foxminded/university_timetable/dao/TimeSlotDao.java
+++ src/main/java/com/foxminded/university_timetable/dao/TimeSlotDao.java
@@ -1,19 +1,14 @@
 package com.foxminded.university_timetable.dao;
 
-import java.sql.Types;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import com.foxminded.university_timetable.model.DailyTimetable;
 import com.foxminded.university_timetable.model.TimeSlot;
 import com.foxminded.university_timetable.row_mapper.TimeSlotRowMapper;
 
@@ -26,6 +21,7 @@ public class TimeSlotDao {
 			+ "VALUES (?, ?, ?, ?, ?, ?)";
 	private static final String UPDATE = "UPDATE time_slots SET start_time = ?, end_time = ?, course_id = ?, teacher_id = ?, group_id = ?, room_id = ?, daily_timetalbe_id = ? WHERE id = ?";
 	private static final String DELETE_BY_ID = "DELETE FROM time_slots WHERE id = ?";
+	private static final String FIND_ALL_TIME_SLOTS_OF_DAILY_TIMETABLE = "SELECT * FROM time_slots WHERE daily_timetable_id = ?";
 	
 	private final JdbcTemplate jdbcTemplate;
 	private final TimeSlotRowMapper timeSlotRowMapper;
@@ -49,37 +45,21 @@ public class TimeSlotDao {
 		return this.jdbcTemplate.query(FIND_ALL, timeSlotRowMapper);
 	}
 	
-	public TimeSlot save(TimeSlot timeSlot) {
-		PreparedStatementCreator psc = 
-				new PreparedStatementCreatorFactory(SAVE, Types.TIME, Types.TIME, Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.INTEGER)
-				.newPreparedStatementCreator(
-						Arrays.asList(
-							timeSlot.getStartTime(),
-							timeSlot.getEndTime(),
-							timeSlot.getCourse().getId(),
-							timeSlot.getTeacher().getId(),
-							timeSlot.getGroup().getId(),
-							timeSlot.getRoom().getId()));
-		KeyHolder keyHolder = new GeneratedKeyHolder();
-		this.jdbcTemplate.update(psc, keyHolder);
-		timeSlot.setId(keyHolder.getKey().longValue());
-		
-		return timeSlot;
+	public void save(TimeSlot timeSlot) {
+		this.jdbcTemplate.update(SAVE, timeSlot.getStartTime(), timeSlot.getEndTime(), timeSlot.getCourse().getId(),
+				timeSlot.getTeacher().getId(), timeSlot.getGroup().getId(), timeSlot.getRoom().getId());
 	}
 
-	public TimeSlot update(TimeSlot timeSlot) {
-		this.jdbcTemplate.update(UPDATE, 
-				timeSlot.getStartTime(), 
-				timeSlot.getEndTime(), 
-				timeSlot.getCourse(), 
-				timeSlot.getTeacher(), 
-				timeSlot.getGroup(), 
-				timeSlot.getRoom(), 
-				timeSlot.getId());
-		return timeSlot;
+	public void update(TimeSlot timeSlot) {
+		this.jdbcTemplate.update(UPDATE, timeSlot.getStartTime(), timeSlot.getEndTime(), timeSlot.getCourse(), timeSlot.getTeacher(), 
+				timeSlot.getGroup(), timeSlot.getRoom(), timeSlot.getId());
 	}
 	
 	public void deleteById(Long id) {
 		this.jdbcTemplate.update(DELETE_BY_ID, id);
+	}
+	
+	public List<TimeSlot> findAllTimeSlotsOfDailyTimetable(DailyTimetable dailyTimetable) {
+		return this.jdbcTemplate.query(FIND_ALL_TIME_SLOTS_OF_DAILY_TIMETABLE, new Object[] {dailyTimetable.getId()}, timeSlotRowMapper);
 	}
 }
--- src/test/java/com/foxminded/university_timetable/dao/TimeSlotDaoTest.java
+++ src/test/java/com/foxminded/university_timetable/dao/TimeSlotDaoTest.java
@@ -1,5 +1,11 @@
 package com.foxminded.university_timetable.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalTime;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +15,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.foxminded.university_timetable.config.TestJdbcConfig;
+import com.foxminded.university_timetable.model.Course;
+import com.foxminded.university_timetable.model.Group;
+import com.foxminded.university_timetable.model.Room;
+import com.foxminded.university_timetable.model.Teacher;
+import com.foxminded.university_timetable.model.TimeSlot;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestJdbcConfig.class})
@@ -17,6 +28,38 @@ class TimeSlotDaoTest {
 
 	@Autowired
 	private TimeSlotDao timeSlotDao;
+	
+	@Autowired
+	private CourseDao courseDao;
+	
+	@Autowired
+	private TeacherDao teacherDao;
+	
+	@Autowired
+	private GroupDao groupDao;
+	
+	@Autowired 
+	private RoomDao roomDao;
+	
+	@Test
+	void givenExistentTimeSlotId_whenFindById_thenReturnOptionalOfTimeSlot() {
+		Optional<Course> course = courseDao.findById(1L);
+		Optional<Teacher> teacher = teacherDao.findById(1L);
+		Optional<Group> group = groupDao.findById(1L);
+		Optional<Room> room = roomDao.findById(1L);
+		TimeSlot timeSlot = new TimeSlot(1L, 
+				LocalTime.of(9, 0), 
+				LocalTime.of(9, 30), 
+				course.orElseThrow(NoSuchElementException::new), 
+				teacher.orElseThrow(NoSuchElementException::new), 
+				group.orElseThrow(NoSuchElementException::new), 
+				room.orElseThrow(NoSuchElementException::new));
+		Optional<TimeSlot> expected = Optional.of(timeSlot);
+		
+		Optional<TimeSlot> actual = timeSlotDao.findById(timeSlot.getId());
+		
+		assertEquals(expected, actual);
+	}
 	
 	@Test
 	void givenTimeSlot_whenFindAll_returnListOfTimeSlots() {		
