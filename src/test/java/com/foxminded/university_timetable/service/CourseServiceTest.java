1611b97285604572c8877a90a93ddd1054228e36
sshkut <sgshkut@gmail.com>
2020-06-16 23:35:14 +0200

    Fix tests for service layer.

--- src/test/java/com/foxminded/university_timetable/service/CourseServiceTest.java
+++ src/test/java/com/foxminded/university_timetable/service/CourseServiceTest.java
@@ -15,16 +15,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.foxminded.university_timetable.config.TestServiceConfig;
 import com.foxminded.university_timetable.dao.CourseDao;
 import com.foxminded.university_timetable.model.Course;
 
-@ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
-@ContextConfiguration(classes = { TestServiceConfig.class })
 class CourseServiceTest {	
 
 	@Mock
